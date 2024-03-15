0769adfbe217c24d2cbd7ab00d3c60fc002082f9
ZIPcde <votmoyyashik100@gmail.com>
2024-03-15 23:17:34 +0300

    initial commit

---
+++ config.py
@@ -0,0 +1,1 @@
+token = "7116777039:AAGwY-OkqgUL84SwqnQ5c2novsUegudS8N0"
---
+++ main.py
@@ -0,0 +1,57 @@
+import asyncio
+import config
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters.command import Command
+import logging
+import random
+from keyboards import kb1, kb2
+
+#Включаем логгирование
+logging.basicConfig(level=logging.INFO)
+
+#Создаем объект бота
+bot = Bot(token=config.token)
+
+#Диспечер
+dp = Dispatcher()
+
+
+
+
+
+#Хэндлер на команду /start
+@dp.message(Command('start'))
+async def cmd_start(message: types.Message):
+    name = message.chat.first_name
+    await message.answer(f'Привет, {name}', reply_markup=kb1)
+
+
+#Хэндлер на команду /stop
+@dp.message(Command('stop'))
+async def cmd_stop(message: types.Message):
+    name = message.chat.first_name
+    await message.answer(f'Пока, {name}')
+
+
+#Хендлер на сообщения
+@dp.message(F.text)
+async def msg_echo(message: types.Message):
+    msg_user = message.text.lower()
+    name = message.chat.first_name
+    if 'привет' in msg_user:
+         await message.answer(f'Привет-привет, {name}')
+    elif 'пока' == msg_user:
+        await message.answer(f'Пока-пока, {name}')
+    elif 'ты кто' in msg_user:
+        await message.answer(f'Я бот, {name}')
+    elif 'лиса' in msg_user:
+        await message.answer(f'Смотри что у меня есть, {name}', reply_markup=kb2)
+    else:
+        await message.answer(f'Я не знаю такого слова')
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
