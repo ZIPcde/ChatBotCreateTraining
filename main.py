9b5b4a5936d786d7a13a931095bfd8bcd0b4ebd8
ZIPcde <votmoyyashik100@gmail.com>
2024-03-15 23:30:07 +0300

    random_fox modified

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from aiogram.filters.command import Command
 import logging
 import random
 from keyboards import kb1, kb2
+from random_fox import fox
 
 #Включаем логгирование
 logging.basicConfig(level=logging.INFO)
@@ -33,17 +34,30 @@ async def cmd_stop(message: types.Message):
     await message.answer(f'Пока, {name}')
 
 
+#Хэндлер на команду /fox
+@dp.message(Command('fox'))
+@dp.message(Command('лиса'))
+@dp.message(F.text.lower() == 'покажи лису')
+async def cmd_fox(message: types.Message):
+    name = message.chat.first_name
+    img_fox = fox()
+    await message.answer(f'Держи лису, {name}')
+    await message.answer_photo(photo=img_fox)
+# await bot.send_photo(message.from_user.id, photo=img_fox)
+
+
+
 #Хендлер на сообщения
 @dp.message(F.text)
 async def msg_echo(message: types.Message):
     msg_user = message.text.lower()
     name = message.chat.first_name
     if 'привет' in msg_user:
-         await message.answer(f'Привет-привет, {name}')
+        await message.answer(f'Привет-привет, {name}')
     elif 'пока' == msg_user:
         await message.answer(f'Пока-пока, {name}')
     elif 'ты кто' in msg_user:
-        await message.answer(f'Я бот, {name}')
+        await message.answer_dice(emoji="🎲")
     elif 'лиса' in msg_user:
         await message.answer(f'Смотри что у меня есть, {name}', reply_markup=kb2)
     else:
